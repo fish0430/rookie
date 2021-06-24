1da3643f0557485c0b6b4ffc13625759eee30943
Yuki <86304641+fish0430@users.noreply.github.com>
2021-06-24 21:06:04 +0800

    Add files via upload

---
+++ prac-oddoreven.py
@@ -0,0 +1,12 @@
+# result=69
+# x=int(input("這期頭獎樂透出來囉... \b請猜看看會是多少呢!!"))
+# for x in (result):
+#     print("答對了!")
+# else:
+#     print("答錯囉!小87")
+
+nums=int(input("輸入一個數字: "))
+if nums%2==1:
+    print("奇數")
+elif nums%2==0:
+    print("偶數")
---
+++ prac2-wordcount.py
@@ -0,0 +1,2 @@
+word=str(input("你心裡在想什麼呢? "))
+print("你用了 %s 個文字敘述內心的想法" %len(word))
